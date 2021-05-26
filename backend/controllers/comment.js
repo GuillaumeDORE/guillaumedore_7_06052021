fe01605ae7a25d753592505145a3746f64a3ddb0
Guillaume DORE <guillaumedore45@gmail.com>
2021-05-26 15:15:30 +0200

    Creation route et ctrl comment

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,2 +1,56 @@
-const connection = require('../connection');
 const Comment = require('../models/Comment');
+
+exports.new_Comment = (req, res, next) => {
+    Comment.insert(req.body.comment.userId, req.body.comment.title, req.body.comment.content, function (err, result, fields) {
+        if (err) {
+            res.status(400).json({err})
+            return console.log(err);
+        }
+        res.status(201).json({message: 'comment créé!'})
+        return console.log(result);
+    });
+};
+
+exports.update_comment = (req, res, next) => {
+    Comment.update(req.body.comment.title, req.body.comment.content, {comment_id: req.params.comment_id}, function (err, result, fields) {
+        if (err) {
+            res.status(400).json({err})
+            return console.log(err);
+        }
+        res.status(200).json({message: 'comment modifié!'})
+        return console.log(result);
+    });
+};
+
+exports.delete_comment = (req, res, next) => {
+    Comment.delete({comment_id: req.params.comment_id}, function (err, result, fields) {
+        if (err) {
+            res.status(400).json({err})
+            return console.log(err);
+        }
+        res.status(200).json({message: 'comment supprimé!!'})
+        return console.log(result);
+    });
+};
+
+exports.get_all_comment = (req, res, next) => {
+    Comment.getAll( function (err, result, fields) {
+        if (err) {
+            res.status(400).json({err})
+            return console.log(err);
+        }
+        res.status(200).json(result[0])
+        return console.log(result);
+    });
+};
+
+exports.get_one_comment = (req, res, next) => {
+    Comment.findOne({comment_id: req.params.comment_id}, function (err, result, fields) {
+        if (err) {
+            res.status(400).json({err})
+            return console.log(err);
+        }
+        res.status(200).json(result[0])
+        return console.log(result);
+    });
+};
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -6,10 +6,10 @@ const commentCtrl = require('../controllers/comment');
 // const {createAccountLimiter} = require('../middleware/rate-limiter');
 
 router.post('/',auth ,/*  createAccountLimiter ,*/ commentCtrl.new_comment);
-router.put('/',auth ,/*  createAccountLimiter ,*/ commentCtrl.update_comment);
-router.delete('/',auth ,/*  createAccountLimiter ,*/ commentCtrl.delete_comment);
+router.put('/:comment_id',auth ,/*  createAccountLimiter ,*/ commentCtrl.update_comment);
+router.delete('/:comment_id',auth ,/*  createAccountLimiter ,*/ commentCtrl.delete_comment);
 router.get('/',auth ,/*  createAccountLimiter ,*/ commentCtrl.get_all_comment);
-router.get('/',auth ,/*  createAccountLimiter ,*/ commentCtrl.get_one_comment);
+router.get('/:comment_id',auth ,/*  createAccountLimiter ,*/ commentCtrl.get_one_comment);
 
 
 module.exports = router;
